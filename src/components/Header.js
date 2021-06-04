949fab25b490a803e40e7fc91b18fcf095e7fa11
rakshith <rakshit.s.gowda@gmail.com>
2021-06-04 20:39:02 +0530

    styles and templating

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,7 @@ export default function Header() {
   return (
     <div>
       <h1>I am header</h1>
+      <h2>I am extra Headers header</h2>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <Link to='/blogs'>Blogs</Link>
